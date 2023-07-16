e248afffbdbdae0eb5ab6e9c8c38f80bec733179
OrFrenkelf1 <or.frenkelf1@gmail.com>
2023-07-16 21:25:18 +0300

    Score

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    //[Header("Players")]
+    //public GameObject player1;
+    //public GameObject player2;
+
+    [Header("Score Text")]
+    public TextMeshProUGUI ScoreText;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //ScoreText.text = ???.ToString();
+    }
+}
